aa9aa45a6ccca323e6046571e45acd37bfe82b46
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-24 09:08:37 -0300

    criação de scroll

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import theme from "./styles/theme";
 import GlobalStyles from "./styles/global";
 import AppRoutes from "./Routes";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <AppRoutes />
