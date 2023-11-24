5ac3cd652bb71e6d939243d44729992c3fcdc5c9
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-24 09:37:20 -0300

    mudando cor do footer

--- src/components/Rodape/styles.js
+++ src/components/Rodape/styles.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const Footer = styled.footer`
-    background-color: ${({theme}) => theme.AZUL_ESCURO};
-    color: ${({theme}) => theme.BRANCO};
-    padding: 2rem 0;
+background-color: ${({ theme }) => theme.COLORS.AZUL_ESCURO};
+color: ${({ theme }) => theme.COLORS.BRANCO};
+padding: 2rem 0;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    column-gap: .5rem;
-`
+display: flex;
+justify-content: center;
+align-items: center;
+column-gap: .5rem;
+`;
